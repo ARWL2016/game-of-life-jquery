5027ecb1effdad365af2dc184b7dc476eaa0dc53
Alistair Willis <alistairrwillis@gmail.com>
2017-02-09 15:10:02 +0900

    added method to dynamically update cell ids

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,34 @@
 $(document).ready(function(){
+
+    var settings = {
+        boardSize: {
+            large: 4900, 
+            medium: 3600, 
+            small: 2500
+        }
+    };
      
     var App = {
         init: function () {
             console.log('app.js loaded');
-            App.generateBoard();
+            console.log('Board size: ' + settings.boardSize.small);
+            App.generateBoard(settings.boardSize.small);
         }, 
-        generateBoard: function () {
+        generateBoard: function (size) {
+            var numberOfCells = size -1; 
+            console.log(numberOfCells);
             var board = $('#board'); 
-            var newCell = '<div class="cell"></div>'; 
-            for (var i=0; i<2499; i++) {
-                board.append(newCell); 
+            var newCell = '<div class="cell"></div>';
+            newCell.title = "title";
+            for (var i=0; i<numberOfCells; i++) {
+                board.append(newCell);
             }
+            $('div.cell').each(function(i){
+                this.id = i; 
+            }); 
             
         }
-
-
-
-
-    }
+    };
 
     App.init(); 
      
